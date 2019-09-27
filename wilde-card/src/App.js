fc2152c5b2a97dda17a26466228885b80d406ea6
github username <github email address>
2019-09-27 10:46:45 -0400

    app dot js title page

--- wilde-card/src/App.js
+++ wilde-card/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {  }
+  render() { 
+    return (
+      <React.Fragment>
+        <h1 class="appTitle"> Wilde Card </h1>
+        <h3 class="appSubtitle"> a creativity application for writers </h3>
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
+ 
